22fd85b10a28bc18a90680cc3838a90ac9fdc0bd
keiza analizi <kas2@discente.ifpe.edu.br>
2021-07-22 00:50:39 -0300

    primeiro commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld!!!");
+		System.out.println("Olá mundo");
+		System.out.println("Keiza Analizi");
+		
+		
+
+	}
+
+}
